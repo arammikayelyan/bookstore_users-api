932333d2905b4c6114d312d984241e657b4974f5
Aram Mikayelyan <aram_mikayelyan@tutanota.com>
2020-07-14 13:51:38 +0400

    This is one way of handling a JSON request from the controller

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,15 +1,40 @@
 package users
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/arammikayelyan/bookstore_users-api/domain/users"
+	"github.com/arammikayelyan/bookstore_users-api/services"
 
 	"github.com/gin-gonic/gin"
 )
 
+// CreateUser func
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement")
+	var user users.User
+
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		return
+	}
+
+	if err := json.Unmarshal(bytes, &user); err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		return
+	}
+
+	c.JSON(http.StatusCreated, result)
 }
 
+// GetUser func
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement")
 }
